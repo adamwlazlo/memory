3ed75574ae58390c1f139b22a3a2010c4cf3bff1
adam <adam.wlazlo@gmail.com>
2022-08-27 17:56:51 +0200

    basic game functionalities ready, with no all error handling...

---
+++ src/Game.java
@@ -0,0 +1,128 @@
+import java.util.*;
+
+public class Game {
+
+    private ArrayList<String> drawnWords;
+    private ArrayList<String> shuffleWords;
+    private ArrayList<Integer> guessedFirstRow;
+    private ArrayList<Integer> guessedSecondRow;
+    private int numberOfWords;
+    private int round;
+
+    public Game() {
+        int level = SelectLevel();
+        drawnWords = new ArrayList<>(); //first row
+        shuffleWords = new ArrayList<>(); //second row
+        guessedFirstRow = new ArrayList<>();
+        guessedSecondRow = new ArrayList<>();
+        if (level == 1) {
+            numberOfWords = 4;
+            round = 10;
+        } else {
+            numberOfWords = 8;
+            round = 15;
+        }
+        ;
+    }
+
+    private void InitializeGame() {
+        Word word = new Word();
+        drawnWords = word.DrawWords(numberOfWords);
+        shuffleWords = new ArrayList<>(drawnWords);
+        Collections.shuffle(shuffleWords);
+
+        for (int i = 0; i < numberOfWords; i++) {
+            guessedFirstRow.add(0);
+            guessedSecondRow.add(0);
+        }
+
+        System.out.println(drawnWords);
+        System.out.println(shuffleWords);
+        System.out.println("\nYou have " + round + " round.");
+
+    }
+
+    public int SelectLevel() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Chose level:\n1 - four word; 10 rounds;\n2 - eight words; 15 rounds\n\n");
+
+        int level = sc.nextInt();
+        return level;
+    }
+
+    public void Play() {
+        int guessedWords = 0;
+        int roundNum = 0;
+        InitializeGame();
+
+        while (guessedWords < numberOfWords && roundNum < round) {
+            PrintGuessedWords(guessedFirstRow, drawnWords);
+            PrintGuessedWords(guessedSecondRow, shuffleWords);
+            System.out.printf("\n");
+            Scanner sc = new Scanner(System.in);
+            System.out.print("\n");
+            System.out.println("\nRound " + (roundNum + 1));
+
+            System.out.print("Select the index of word to display in firs row\n");
+
+            int firstDigit = sc.nextInt();
+            if (firstDigit < 1 || firstDigit > numberOfWords) {
+                System.out.println("Enter a number from 1 to " + numberOfWords);
+                continue;
+            }
+
+            PrintGivenWords(drawnWords, firstDigit);
+
+            System.out.printf("\n");
+            System.out.print("Select the index of word to display in second row\n");
+            int secondDigit = sc.nextInt();
+
+            if (secondDigit < 1 || secondDigit > numberOfWords) {
+                System.out.println("Enter a number from 1 to " + numberOfWords);
+                continue;
+            }
+
+            PrintGivenWords(shuffleWords, secondDigit);
+
+            if (drawnWords.get(firstDigit - 1) == shuffleWords.get(secondDigit - 1)) {
+                guessedWords++;
+                guessedFirstRow.set(firstDigit - 1, 1);
+                guessedSecondRow.set(secondDigit - 1, 1);
+            } else {
+                System.out.println("\n\nGiven words doesn't match");
+            }
+
+            roundNum++;
+        }
+        if (guessedWords == numberOfWords)
+            System.out.println("WIN!");
+        else
+            System.out.println("LOOSE!");
+
+    }
+
+    private void PrintGuessedWords(ArrayList<Integer> guessedRow, ArrayList<String> drawnWords) {
+        String displayWords = "";
+
+        for (int i = 0; i < numberOfWords; i++) {
+            if (guessedRow.get(i) == 0) {
+                displayWords = displayWords + "    " + "X";
+            } else {
+                displayWords = displayWords + "    " + drawnWords.get(i);
+            }
+        }
+        System.out.println(displayWords);
+    }
+
+    private void PrintGivenWords(ArrayList<String> drawnWords, int index) {
+        for (int i = 0; i < numberOfWords; i++) {
+            if (i != index - 1)
+                System.out.printf(" X ");
+            else
+                System.out.printf(drawnWords.get(i));
+        }
+
+        System.out.printf("\n\n");
+    }
+
+}
---
+++ src/Word.java
@@ -0,0 +1,57 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Word {
+    public List<String> wordsArray;
+
+    public Word() {
+        wordsArray = new ArrayList<>();
+        ReadFile();
+
+    }
+
+    public void ReadFile()
+    {
+        try {
+            wordsArray = new ArrayList<>();
+            File myObj = new File("C:\\Users\\adam\\IdeaProjects\\memory\\src\\Words.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                assert false;
+                this.wordsArray.add(myReader.nextLine());
+            }
+            System.out.println(this.wordsArray);
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> DrawWords(int number_of_words)
+    {
+        ArrayList<String> arr_drawn_words = new ArrayList<>();
+
+        for(int i = 0; i < number_of_words; i++)
+        {
+            Random rand = new Random();
+            int randNum = rand.nextInt(0, wordsArray.size());
+
+            while (arr_drawn_words.contains(wordsArray.get(randNum)))
+            {
+                randNum = rand.nextInt(0, wordsArray.size());
+
+            }
+
+            arr_drawn_words.add(wordsArray.get(randNum));
+
+        }
+        return arr_drawn_words;
+    }
+
+}
